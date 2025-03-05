398c57c42c6da68c759640d1ce63fb1332febe1b
Ryan Nargesi <hamed.nargesi.jar@gmail.com>
2025-03-06 01:14:58 +0330

    login page

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {
   ButtonDirective,
   CardBodyComponent,
@@ -9,6 +10,8 @@ import {
   ContainerComponent,
   FormControlDirective,
   FormDirective,
+  FormFeedbackComponent,
+  GutterDirective,
   InputGroupComponent,
   InputGroupTextDirective,
   RowComponent,
@@ -16,13 +19,17 @@ import {
 } from '@coreui/angular';
 import { cilLockLocked, cilUser } from '@coreui/icons';
 import { IconDirective } from '@coreui/icons-angular';
-import { TranslationPipe } from '../@services';
+import { AuthService, TranslationPipe } from '../@services';
+import { result } from 'lodash-es';
+import { ApiResult } from '../@models';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   imports: [
+    FormsModule,
+    NgStyle,
     ContainerComponent,
     RowComponent,
     ColComponent,
@@ -36,10 +43,27 @@ import { TranslationPipe } from '../@services';
     InputGroupTextDirective,
     IconDirective,
     FormControlDirective,
+    FormFeedbackComponent,
     ButtonDirective,
-    NgStyle,
+    GutterDirective,
   ],
 })
 export class LoginComponent {
   icons = { cilUser, cilLockLocked };
+  isValidated = false;
+  username?: string;
+  password?: string;
+  result?: ApiResult;
+
+  constructor(private readonly service: AuthService) {}
+
+  login() {
+    this.isValidated = true;
+
+    if (!this.username || !this.password) return;
+
+    this.service
+      .login(this.username, this.password)
+      .subscribe((result) => (this.result = result));
+  }
 }
