0eed63792adb73586ba3c48974640e55591435a6
Hamed Nargesi <hamedna@systemgroup.net>
2025-04-09 18:09:08 +0330

    account controller basic

---
+++ API/Controllers/AccountContoller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotonBypass.API.Models;
+
+namespace PhotonBypass.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountContoller(ILogger<AccountContoller> logger) : ControllerBase
+{
+    private readonly ILogger<AccountContoller> logger = logger;
+
+    [HttpPost("change-ovpn")]
+    public ActionResult ChangeOVPN(ChangeOvpnContext context)
+    {
+        return Ok();
+    }
+}
---
+++ API/Models/ChangeOvpnContext.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotonBypass.API.Models;
+
+public class ChangeOvpnContext
+{
+    public string? Token { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Target { get; set; }
+}
