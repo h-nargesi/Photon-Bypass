970f430a3f7e0ac95d6e03957a844d0a0f3efa7f
Ryan Nargesi <hamed.nargesi.jar@gmail.com>
2025-04-10 19:00:38 +0330

    Organaize Backend

---
+++ Backend/API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotonBypass.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(ILogger<AuthController> logger) : ControllerBase
+{
+    private readonly ILogger<AuthController> logger = logger;
+
+
+}
---
+++ Backend/API/Controllers/BasicsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotonBypass.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BasicsController(ILogger<BasicsController> logger) : ControllerBase
+{
+    private readonly ILogger<BasicsController> logger = logger;
+
+
+}
