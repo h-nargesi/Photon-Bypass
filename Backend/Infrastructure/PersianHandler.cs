9e8188f6a7fe63cbb50e2766c9b1e837b61d0f5e
Ryan Nargesi <hamed.nargesi.jar@gmail.com>
2025-04-12 22:33:59 +0330

    persian date

---
+++ Backend/Infrastructure/PersianHandler.cs
@@ -0,0 +1,11 @@
+﻿using Photon.Persian;
+
+namespace PhotonBypass.Infra;
+
+public static class PersianHandler
+{
+    public static string ToPersianString(this DateTime date)
+    {
+        return new Jalali(date).GetDate().ToNumberic();
+    }
+}
