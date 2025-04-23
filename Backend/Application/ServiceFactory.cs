19e52d6a75b562dc2d9433612aa0318bf04f97bd
Hamed Nargesi <hamedna@systemgroup.net>
2025-04-23 11:14:09 +0330

    application dependancy

--- Backend/Application/ServiceFactory.cs
+++ Backend/Application/ServiceFactory.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PhotonBypass.Application.Account;
 using PhotonBypass.Application.Authentication;
+using PhotonBypass.Application.Basics;
 using PhotonBypass.Application.Connection;
+using PhotonBypass.Application.Plan;
+using PhotonBypass.Application.Vpn;
 
 namespace PhotonBypass.Application;
 
@@ -11,6 +14,9 @@ public static class ServiceFactory
     {
         services.AddScoped<IAccountApplication, AccountApplication>();
         services.AddScoped<IAuthApplication, AuthApplication>();
+        services.AddScoped<IBasicsApplication, BasicsApplication>();
         services.AddScoped<IConnectionApplication, ConnectionApplication>();
+        services.AddScoped<IPlanApplication, PlanApplication>();
+        services.AddScoped<IVpnApplication, VpnApplication>();
     }
 }
